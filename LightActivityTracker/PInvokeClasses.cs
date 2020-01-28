aaf3670e2e4e02b1dc653902e4bd885d8797b856
Max <dr.maximilian.scherer@gmail.com>
2020-01-28 18:07:41 +0100

    initial commit

---
+++ LightActivityTracker/PInvokeClasses.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+
+namespace LightActivityTracker
+{
+    public static class WindowWatcher
+    {
+        private static string _activeWindowTitle;
+        private static string _activeWindowProcess;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+
+        /// <summary>
+        /// unused... we could also take screenshots and maybe track average color or something like that
+        /// </summary>
+        public static void GetActiveWindowColor()
+        {
+            //take screenshot of active window
+            IntPtr handle = GetForegroundWindow();
+            RECT rect;
+            GetWindowRect(handle, out rect);
+            Rectangle bounds = new Rectangle(
+                (int)(rect.Left / (double)Screen.PrimaryScreen.Bounds.Width * 3840.0),
+                (int)(rect.Top / (double)Screen.PrimaryScreen.Bounds.Height * 2160.0),
+                (int)((rect.Right - rect.Left + 1) / (double)Screen.PrimaryScreen.Bounds.Width * 3840.0),
+                (int)((rect.Bottom - rect.Top + 1) / (double)Screen.PrimaryScreen.Bounds.Height * 2160.0)
+                );
+
+            
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(bounds.Left, bounds.Top, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
+                }
+                bitmap.Save(Path.Combine(Tracker.myPath, "test.png"), System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        public static void GetActiveWindowTitleAndProcess()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            uint pid;
+            GetWindowThreadProcessId(handle, out pid);
+            Process p = Process.GetProcessById((int)pid);
+            GetWindowText(handle, Buff, nChars);
+            _activeWindowTitle = Buff.ToString();
+
+            _activeWindowProcess = p.ProcessName;// .MainModule.ModuleName;
+
+        }
+
+        public static string ActiveWindowTitle { get { return _activeWindowTitle; } }
+        public static string ActiveWindowProcess { get { return _activeWindowProcess; } }
+    }
+
+    /// <summary>
+    /// Global KeyboardHook to monitor for KeyPresses (for AFK detection)
+    /// </summary>
+    public static class KeyboardHook
+    {
+        // mainly taken from https://stackoverflow.com/questions/3654787/global-hotkey-in-console-application
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        ////////////////////////////////////////////////////////////////
+        //Some constants to make handling our hook code easier to read
+        ////////////////////////////////////////////////////////////////
+        private const int WH_KEYBOARD_LL = 13;                    //Type of Hook - Low Level Keyboard
+        private const int WM_KEYDOWN = 0x0100;                    //Value passed on KeyDown
+        private const int WM_KEYUP = 0x0101;                      //Value passed on KeyUp
+        private static LowLevelKeyboardProc _proc = HookCallback; //The function called when a key is pressed
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        public static event EventHandler<int> HotKeyPressed;
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) //A Key was pressed down
+            {
+                int vkCode = Marshal.ReadInt32(lParam);           //Get the keycode
+                                                                  //string theKey = ((Keys)vkCode).ToString();        //Name of the key
+
+                HotKeyPressed?.Invoke(typeof(Program), vkCode);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam); //Call the next hook
+        }
+
+        static KeyboardHook()
+        {
+            _hookID = SetHook(_proc);  //Set our hook
+        }
+    }
+
+    /// <summary>
+    /// Global MouseHook to monitor for mouse events (for AFK detection)
+    /// </summary>
+    public static class MouseHook
+
+    {
+        public static event EventHandler MouseAction = delegate { };
+
+        static MouseHook()
+        {
+            _hookID = SetHook(_proc);
+        }
+
+        public static void stop()
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private static LowLevelMouseProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static IntPtr SetHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                  GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+          int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam || MouseMessages.WM_RBUTTONDOWN == (MouseMessages)wParam || MouseMessages.WM_MOUSEWHEEL == (MouseMessages)wParam))
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                MouseAction(null, new EventArgs());
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        private const int WH_MOUSE_LL = 14;
+
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+          LowLevelMouseProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+          IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+
+    }
+}
---
+++ LightActivityTracker/Program.cs
@@ -0,0 +1,245 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace LightActivityTracker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var track = new Tracker();
+
+            AppDomain.CurrentDomain.UnhandledException += track.AppDomain_CurrentDomain_UnhandledException;
+            Application.Run();
+        }
+    }
+
+    public class ActivityEvent
+    {
+        public DateTime dt { get; set; }
+        public string Process { get; set; }
+        public string Title { get; set; }
+        public bool IsAFK_Event { get; set; }
+    }
+
+
+    public class Filter
+    {
+        public string ProcessRegEx { get; set; }
+        public string TitleRegEx { get; set; }
+        public string RedactedProcess { get; set; }
+        public string RedactedTitle { get; set; }
+    }
+
+
+    public class Tracker
+    {
+        ActivityEvent lastActivitiy;
+        public void AppDomain_CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // use logger here to log the events exception object
+            // before the application quits
+            _log.Error(e.ExceptionObject.ToString());
+            
+        }
+
+        public static string myPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        public Logger _log;
+
+        Thread window_watcher;
+
+        DateTime lastAction;
+
+        Filter[] filter_expressions;
+
+        void LogActivity(ActivityEvent act)
+        {
+            var f = Path.Combine(myPath, "activities.csv");
+            _log.Info("activity change detected");
+
+            bool isNew = File.Exists(f);
+            lock (this)
+            {
+                using (var writer = new StreamWriter(f, true))
+                using (var csv = new CsvHelper.CsvWriter(writer, new CsvHelper.Configuration.Configuration() { Encoding = Encoding.UTF8, HasHeaderRecord = !isNew, CultureInfo = System.Globalization.CultureInfo.CurrentCulture }))
+                {
+                    csv.Configuration.TypeConverterOptionsCache.GetOptions<DateTime>().Formats = new[] { "yyyy-MM-dd HH:mm:ss.fff" };
+                    csv.WriteRecords(new List<ActivityEvent>() { act });
+                    csv.Flush();
+                }
+            }
+            //activity change
+            lastActivitiy = act;
+        }
+
+        public Tracker()
+        {
+            //detect log on/off events
+            Microsoft.Win32.SystemEvents.PowerModeChanged += new Microsoft.Win32.PowerModeChangedEventHandler((o, e) =>
+            {
+                var act = new ActivityEvent() { dt = DateTime.Now, Process = "Manual_PowerModeChanged", IsAFK_Event = true };
+                switch (e.Mode)
+                {
+                    case Microsoft.Win32.PowerModes.Resume:
+                        _log.Info("resume");
+                        act.Title = "resume";
+                        break;
+                    case Microsoft.Win32.PowerModes.Suspend:
+                        _log.Info("sleep");
+                        act.Title = "sleep";
+                        break;
+                }
+                LogActivity(act);
+            });
+
+            Microsoft.Win32.SystemEvents.SessionEnding += new Microsoft.Win32.SessionEndingEventHandler((o, e) =>
+            {
+                var act = new ActivityEvent() { dt = DateTime.Now, Process = "Manual_SessionEnding", IsAFK_Event = true };
+
+                switch (e.Reason)
+                {
+                    case Microsoft.Win32.SessionEndReasons.Logoff:
+                        _log.Info("logoff");
+                        act.Title = "logoff";
+                        break;
+                    case Microsoft.Win32.SessionEndReasons.SystemShutdown:
+                        _log.Info("shutdown");
+                        act.Title = "shutdown";
+                        break;
+                }
+                LogActivity(act);
+            });
+
+
+           filter_expressions = Newtonsoft.Json.JsonConvert.DeserializeObject<Filter[]>(File.ReadAllText(Path.Combine(myPath, "filters.json"), Encoding.UTF8));
+            lastAction = DateTime.Now;
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = System.IO.Path.Combine(myPath, "log_file.txt"), ArchiveAboveSize = 5*1024*1024, MaxArchiveFiles = 2 };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+            
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+            
+            _log = NLog.LogManager.GetCurrentClassLogger();
+
+            lastActivitiy = null;
+
+            var f = Path.Combine(myPath, "activities.csv");
+            //backup
+            if (File.Exists(f))
+                File.Copy(f, f + ".bak", true);
+
+            NotifyIcon trayIcon = new NotifyIcon
+            {
+                Text = "ActivityWatch",
+                Icon = new Icon("icon.ico", 64, 64)
+            };
+            trayIcon.Text = $"LightActivityTracker";
+            ContextMenu trayMenu = new ContextMenu();
+
+            trayMenu.MenuItems.Add("Quit", new EventHandler((o, e) => 
+            {
+                _log.Info("exiting");
+                var act = new ActivityEvent() { dt = DateTime.Now, Process = "Manual_Exit", IsAFK_Event = true };
+                LogActivity(act);
+                Environment.Exit(1);
+            }));
+
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+
+            window_watcher = new Thread(new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        WindowWatcher.GetActiveWindowTitleAndProcess();
+                    }
+                    catch (Exception ex)
+                    {
+                        _log.Error($"error in get active window {ex.ToString()}");
+                    }
+                    
+
+                    //_log.Info($"{WindowWatcher.ActiveWindowProcess} - {WindowWatcher.ActiveWindowTitle}");
+                    //log events on window change
+                    //log afk event if afk
+                    
+
+                    var act = new ActivityEvent()
+                    {
+                        dt = DateTime.Now,
+                        IsAFK_Event = false,
+                        Process = WindowWatcher.ActiveWindowProcess,
+                        Title = WindowWatcher.ActiveWindowTitle
+                    };
+
+                    if ((DateTime.Now - lastAction).TotalSeconds > 60)
+                    {
+                        _log.Info($"afk since {lastAction.ToShortTimeString()}");
+                        act.dt = lastAction;
+                        if (act.dt < lastActivitiy.dt)
+                            act.dt = lastActivitiy.dt + TimeSpan.FromSeconds(1);
+                        act.IsAFK_Event = true;
+                    }
+
+                    //filter
+                    foreach (var filter in filter_expressions)
+                    {
+                        if (Regex.IsMatch(act.Process, filter.ProcessRegEx, RegexOptions.IgnoreCase) &&
+                            Regex.IsMatch(act.Title, filter.TitleRegEx, RegexOptions.IgnoreCase))
+                        {
+                            act.Title = filter.RedactedTitle;
+                            act.Process = filter.RedactedProcess;
+                        }
+                    }
+                    
+                    if (lastActivitiy == null || lastActivitiy.Process != act.Process || lastActivitiy.Title != act.Title || lastActivitiy.IsAFK_Event != act.IsAFK_Event)
+                    {
+                        LogActivity(act);
+                    }
+
+
+                    Thread.Sleep(450);
+                }
+            }));
+
+            window_watcher.Start();
+
+
+            KeyboardHook.HotKeyPressed += new EventHandler<int>((sender, e) => {
+                //_log.Info("keypress");
+                lastAction = DateTime.Now;
+            });
+
+#if !DEBUG
+            MouseHook.MouseAction += new EventHandler((object sender, EventArgs e) => {
+                //_log.Info("mouse click");
+                lastAction = DateTime.Now;
+            });
+#endif
+
+            var act2 = new ActivityEvent() { dt = DateTime.Now, Process = "Manual_Startup", IsAFK_Event = true };
+            LogActivity(act2);
+        }
+
+
+    }
+
+}
